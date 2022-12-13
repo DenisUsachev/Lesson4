7d57b5869d9a151fbc4b5a9d4622affe509ea831
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-13 20:47:21 +0500

    Add commit

---
+++ tesk3/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Задача 28: Напишите программу, которая принимает на вход число
+ N и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120*/
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while (!isCorrect)
+    {
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Введите число больше 0");
+        }
+    }
+    return result;
+}
+
+long GetSumm(int number)
+{
+    long summ = 1;
+
+    for (int i = 1; i <= number; i++)
+    {
+        summ *= i;
+    }
+    return summ;
+}
+
+
+
+int number = GetNumber("Введите число больше 0");
+long summ = GetSumm(number);
+
+Console.WriteLine($"Сумма чисел от {number} = {summ}");
